23d9e64cb9fa55848ae69173d27ea660566e66ae
Philipp Hofmann <philipp.hofmann@commercetools.com>
2023-01-19 17:17:52 +0100

    closer to existing components

--- src/components/types/field-definition-input/FieldDefinitionInputForm.tsx
+++ src/components/types/field-definition-input/FieldDefinitionInputForm.tsx
@@ -2,7 +2,6 @@ import { ReactElement, FC } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { useFormik, type FormikHelpers, FormikErrors } from 'formik';
 import omitEmpty from 'omit-empty-es';
-import Card from '@commercetools-uikit/card';
 import CheckboxInput from '@commercetools-uikit/checkbox-input';
 import { customProperties } from '@commercetools-uikit/design-system';
 import Grid from '@commercetools-uikit/grid';
@@ -38,7 +37,7 @@ export type TFormValues = {
   required: boolean;
   inputHint: string;
   type: { name: string; referenceTypeId: string };
-}; //& TFieldDefinition;
+};
 
 type TErrors = {
   key: { missing?: boolean };
@@ -93,74 +92,69 @@ const FieldDefinitionInputForm: FC<Props> = ({
   const formElements = (
     <Spacings.Stack scale="m">
       <Grid
-        gridTemplateColumns={`repeat(2, ${customProperties.constraint11})`}
+        gridTemplateColumns={`repeat(1, ${customProperties.constraint11})`}
         gridGap={customProperties.spacingM}
       >
         <Grid.Item>
-          <Card type="flat" theme="dark">
-            <TextField
-              name="name"
-              hint={<FormattedMessage {...messages.nameHint} />}
-              value={formik.values.name}
-              title={<FormattedMessage {...messages.nameTitle} />}
-              isRequired
-              touched={formik.touched.name ? true : false}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-            />
-            {/* {errors.name && touched.name ? (
+          <TextField
+            name="name"
+            hint={<FormattedMessage {...messages.nameHint} />}
+            value={formik.values.name}
+            title={<FormattedMessage {...messages.nameTitle} />}
+            isRequired
+            touched={formik.touched.name ? true : false}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {/* {errors.name && touched.name ? (
               <ErrorMessage>{errors.name}</ErrorMessage>
             ) : null} */}
-          </Card>
         </Grid.Item>
         <Grid.Item>
-          <Card type="flat" theme="dark">
-            <LocalizedTextField
-              name="label"
-              selectedLanguage={dataLocale}
-              value={formik.values.label}
-              title={<FormattedMessage {...messages.labelTitle} />}
-              isRequired
-              touched={formik.touched.label ? true : false}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              renderError={(key, error) => error}
-            />
-          </Card>
+          <LocalizedTextField
+            name="label"
+            selectedLanguage={dataLocale}
+            value={formik.values.label}
+            title={<FormattedMessage {...messages.labelTitle} />}
+            isRequired
+            touched={formik.touched.label ? true : false}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
         </Grid.Item>
         <Grid.Item>
-          <Card type="flat" theme="dark">
-            <SelectField
-              name="type.name"
-              title={<FormattedMessage {...messages.typeTitle} />}
-              isRequired
-              value={formik.values.type.name}
-              options={fieldTypes}
-              touched={formik.touched.type?.name}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              isDisabled={editMode}
-            />
-          </Card>
-          {
-            // Only display 'reference' drop-down if reference type selected.
-            formik.values.type.name === 'Reference' && (
-              <Card type="flat" theme="dark">
-                <SelectField
-                  name="type.referenceTypeId"
-                  title={<FormattedMessage {...messages.referenceTitle} />}
-                  isRequired
-                  value={formik.values.type.referenceTypeId}
-                  options={referenceTypeOptions}
-                  // touched={touched.type?.referenceTypeId}
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  isDisabled={editMode}
-                />
-              </Card>
-            )
-          }
-          <Card type="flat" theme="dark">
+          <SelectField
+            name="type.name"
+            title={<FormattedMessage {...messages.typeTitle} />}
+            isRequired
+            value={formik.values.type.name}
+            options={fieldTypes}
+            touched={formik.touched.type?.name}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            isDisabled={editMode}
+          />
+        </Grid.Item>
+        {
+          // Only display 'reference' drop-down if reference type selected.
+          formik.values.type.name === 'Reference' && (
+            <Grid.Item>
+              <SelectField
+                name="type.referenceTypeId"
+                title={<FormattedMessage {...messages.referenceTitle} />}
+                isRequired
+                value={formik.values.type.referenceTypeId}
+                options={referenceTypeOptions}
+                // touched={touched.type?.referenceTypeId}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                isDisabled={editMode}
+              />
+            </Grid.Item>
+          )
+        }
+
+        {/* <Card type="flat" theme="dark">
             <CheckboxInput
               name="isSet"
               onChange={formik.handleChange}
@@ -168,28 +162,27 @@ const FieldDefinitionInputForm: FC<Props> = ({
             >
               <FormattedMessage {...messages.setTitle} />
             </CheckboxInput>
-          </Card>
-          <Card type="flat" theme="dark">
-            <CheckboxInput
-              name="required"
-              onChange={formik.handleChange}
-              isChecked={formik.values.required}
-            >
-              <FormattedMessage {...messages.requiredTitle} />
-            </CheckboxInput>
-          </Card>
-          <Card type="flat" theme="dark">
-            <SelectField
-              name="inputHint"
-              title={<FormattedMessage {...messages.inputHintTitle} />}
-              value={formik.values.inputHint}
-              options={inputHints}
-              touched={formik.touched.inputHint}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              isDisabled={editMode}
-            />
-          </Card>
+          </Card> */}
+        <Grid.Item>
+          <CheckboxInput
+            name="required"
+            onChange={formik.handleChange}
+            isChecked={formik.values.required}
+          >
+            <FormattedMessage {...messages.requiredTitle} />
+          </CheckboxInput>
+        </Grid.Item>
+        <Grid.Item>
+          <SelectField
+            name="inputHint"
+            title={<FormattedMessage {...messages.inputHintTitle} />}
+            value={formik.values.inputHint}
+            options={inputHints}
+            touched={formik.touched.inputHint}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            isDisabled={editMode}
+          />
         </Grid.Item>
       </Grid>
     </Spacings.Stack>

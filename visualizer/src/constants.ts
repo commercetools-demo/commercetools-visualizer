5e5b71e0869094433a48b0cdc555deabc696be59
Philipp Hofmann <philipp.hofmann@commercetools.com>
2024-01-16 08:33:20 +0100

    on its way to become a connect app

--- visualizer/src/constants.ts
+++ visualizer/src/constants.ts
@@ -1,9 +1,12 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
+declare const window: Window &
+  typeof globalThis & {
+  app: { entryPointUriPath: string };
+};
+
 export const entryPointUriPath =
-  typeof window === 'undefined'
-    ? process.env.ENTRY_POINT_URI_PATH
-    : window.app.entryPointUriPath;
+  typeof window === 'undefined' ? 'visualizer' : window.app.entryPointUriPath;
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);

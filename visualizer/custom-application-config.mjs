30d98b7d24006c6e0f89964420fe0749ed40fdec
Behnam Tehrani <behnam777@gmail.com>
2026-02-13 15:06:17 -0500

    fix Entity Diagram: update config

--- visualizer/custom-application-config.mjs
+++ visualizer/custom-application-config.mjs
@@ -96,6 +96,11 @@ const config = {
       labelAllLocales: [],
       // permissions: [PERMISSIONS.View],
     },
+    {
+      uriPath: 'entity-diagram',
+      defaultLabel: 'Entity Diagram',
+      labelAllLocales: [],
+    },
   ],
 };
 
